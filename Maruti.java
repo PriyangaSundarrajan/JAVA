213c4881f606a0b3f5ec6e278797a08ebca98a07
PriyangaSundarrajan <priyanivi96@gmail.com>
2022-10-27 23:35:29 +0530

    Maruti.java

---
+++ Maruti.java
@@ -0,0 +1,23 @@
+class Maruti extends Car
+    {
+        public static void main(String[] args)
+    {
+
+    }
+  void maintain_mileage()
+    {
+        System.out.println("Super Mileage Car");
+    }
+
+void accelerate()
+    {
+        System.out.println("Maruti High Speed"); 
+    }
+
+
+void applyBrakes()
+    {
+        System.out.println("Maruti Stopping"); 
+    }
+
+}
